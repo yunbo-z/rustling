b27cbad90a08201cbf943f3d5b45415d4bf51c14
yunbo-z <zyunbo0330@gmail.com>
2025-01-09 13:55:20 +0100

    strings and modules

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut input_string: String = input.to_string();
+    input_string.push_str(" world!");
+    input_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
